81506670d5b43d8c4ba9a3fe564d2f244a435789
Corneliu <29034380+mishimomei@users.noreply.github.com>
2023-06-19 13:42:17 +0300

    added basic html content for tasks 1/2, added buildGrid function, creates X rows, then adds X columns to each row, done tests on function, added css to display equal square elements grid

---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('#container');
+
+//function to build the square grid with X row/columns
+function buildGrid(x) {
+    for (let i = 0; i < x; i++) {
+        const row = document.createElement('div');
+        row.classList.add('rowElement');
+        container.appendChild(row);
+    }
+
+
+    const rows = document.querySelectorAll('.rowElement');
+    rows.forEach((row) => {
+        for (let i = 0; i < x; i++) {
+            const column = document.createElement('div');
+            column.classList.add('columnElement');
+            row.appendChild(column);
+        }
+    });
+}
+
+buildGrid(16);
